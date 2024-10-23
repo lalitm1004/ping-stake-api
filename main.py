ac81117da7245086193983b5368b8b888c72a953
Lalit M <lalitm1004@gmail.com>
2024-10-23 11:47:45 +0530

    add requirements

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ class GambleResult(BaseModel):
 async def gamble(amount_bet: float, win_percentage: float):
     won = random.random() < win_percentage / 100
     amount_won = amount_bet * 100 / win_percentage if won else 0
-    
+
     return GambleResult(
         amount_bet=amount_bet,
         win_percentage=win_percentage,
         won=won,
         amount_won=amount_won
-    )   
+    )
